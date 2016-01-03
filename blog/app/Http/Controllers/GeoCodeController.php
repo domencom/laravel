9d37d7613aa6bc77ef5c9173f7b63198f1c89980
Ruslan <buyhey@yandex.ru>
2016-01-03 17:31:23 +0200

    refactor test

--- blog/app/Http/Controllers/GeoCodeController.php
+++ blog/app/Http/Controllers/GeoCodeController.php
@@ -6,29 +6,26 @@ use App\Services\GeoCode\Request;
 
 class GeoCodeController extends Controller {
 
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index(Manager $manager)
-	{
-		$req1 = (new Request())
-			->setAddress('1600+Amphitheatre+Parkway,+Mountain+View,+CA')
-			->setLanguage('en');
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index(Manager $manager)
+    {
+        $req1 = (new Request())
+            ->setAddress('1600+Amphitheatre+Parkway,+Mountain+View,+CA')
+            ->setLanguage('en');
 
-		// reverse geo coding
-		$req2 = (new Request())
-			->setLatLong(40.71, -73.96)
-			->setLanguage('ru');
+        // reverse geo coding
+        $req2 = (new Request())
+            ->setLatLong(40.71, -73.96)
+            ->setLanguage('ru');
 
-		$res1 = json_decode($manager->sendRequest($req1)->toJson());
-		$res2 = json_decode($manager->sendRequest($req2)->toJson());
+        $res1 = json_decode($manager->sendRequest($req1)->toJson());
+        $res2 = json_decode($manager->sendRequest($req2)->toJson());
 
-		dd($res1, $res2);
-		return view('geocode.index');
-	}
-
-
-
+        dd($res1, $res2);
+        return view('geocode.index');
+    }
 }
--- blog/app/Providers/GeoCodeServiceProvider.php
+++ blog/app/Providers/GeoCodeServiceProvider.php
@@ -6,31 +6,30 @@ use App\Services\GeoCode;
 
 class GeoCodeServiceProvider extends ServiceProvider {
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = true;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
 
 
-	/**
-	 * Register the application services.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		$this->app->singleton(GeoCode\Manager::class, function($app) {
-			$config = (new GeoCode\Config())->initFromArray(config('services.geo_code'));
-			$req = new GeoCode\Http\Guzzle(['base_uri' => $config->apiUrl, 'verify' => false]);
-			return new GeoCode\Manager($config, $req, Cache::store($config->cacheDriver));
-		});
-	}
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton(GeoCode\Manager::class, function($app) {
+            $config = (new GeoCode\Config())->initFromArray(config('services.geo_code'));
+            $req = new GeoCode\Http\Guzzle(['base_uri' => $config->apiUrl, 'verify' => false]);
+            return new GeoCode\Manager($config, $req, Cache::store($config->cacheDriver));
+        });
+    }
 
-	public function provides()
-	{
-		return [GeoCode\Manager::class];
-	}
-
+    public function provides()
+    {
+        return [GeoCode\Manager::class];
+    }
 }
