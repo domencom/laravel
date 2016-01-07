cae4bbcd93cb6b172edb5a8e4188925ad7ba4bb6
Ruslan <buyhey@yandex.ru>
2016-01-07 11:29:34 +0200

    asdfsdf

--- blog/app/Http/Requests/CreateTask.php
+++ blog/app/Http/Requests/CreateTask.php
@@ -4,26 +4,26 @@ use App\Http\Requests\Request;
 
 class CreateTask extends Request {
 
-	/**
-	 * Determine if the user is authorized to make this request.
-	 *
-	 * @return bool
-	 */
-	public function authorize()
-	{
-		return true;
-	}
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
 
-	/**
-	 * Get the validation rules that apply to the request.
-	 *
-	 * @return array
-	 */
-	public function rules()
-	{
-		return [
-			'name' => 'required|max:10'
-		];
-	}
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|max:10'
+        ];
+    }
 
 }
